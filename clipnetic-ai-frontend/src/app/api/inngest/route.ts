0892d7a6e91980f4db94689274631a5f2a46a612
Soumo Sarkar <soumosarkar297@gmail.com>
2025-06-23 15:45:55 +0530

    fix: enables the signingKey which is required for Inngest production authentication

--- clipnetic-ai-frontend/src/app/api/inngest/route.ts
+++ clipnetic-ai-frontend/src/app/api/inngest/route.ts
@@ -1,12 +1,12 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import { processVideo } from "~/inngest/functions";
+import { env } from "~/env";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [processVideo],
-  // signingKey: env.INNGEST_SIGNING_KEY,
+  signingKey: env.INNGEST_SIGNING_KEY,
   // serveHost: env.INNGEST_SERVE_HOST ?? "localhost:3000",
-  // servePath: env.INNGEST_SERVE_PATH ?? "/api/inngest",
 });
